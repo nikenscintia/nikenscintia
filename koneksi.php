9dfd537e92f10b5007a46f39059e92ee6cd6bc0e
nikenscintia <61862745+nikenscintia@users.noreply.github.com>
2020-03-13 14:51:21 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php 
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DB', 'id306409_praktikum');
+
+$con = mysql_connect(HOST,USER,PASS,DB) or die ('Unable to Connect'); ?>
